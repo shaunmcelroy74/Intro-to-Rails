cebc20ab0ac5fdcc783cc6beebeeb42ea655fcbe
shaunmcelroy74 <shaunmcelroy74@gmail.com>
2025-03-18 17:58:23 -0500

    Added a partial for movies and used it in the index and show views

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -4,15 +4,10 @@ class Publisher < ApplicationRecord
   validates :publisher_name, presence: true, uniqueness: true
 
   def self.order_by_book_count
-    #     SELECT count(publishers.id) as book_count, publishers.*
-    # FROM Publishers
-    #   LEFT OUTER JOIN books on books.publisher_id = publishers.id
-    # GROUP BY publishers.id
-    # ORDER BY book_count DESC;
-    self.select("publishers.*")
-        .select("count(publishers.id) as book_count")
-        .left_joins(:books)
-        .group("publishers.id")
-        .order("book_count DESC")
+    select("publishers.*")
+      .select("count(books.id) as books_count")
+      .left_joins(:books)
+      .group("publishers.id")
+      .order("books_count DESC")
   end
 end
